f4451e1b709824098263db0a95de9a86eac4a737
Aditya Vaddiraju <63994674+aditya2104@users.noreply.github.com>
2023-11-26 10:00:35 +0530

    commit 3
    app functional

--- main.py
+++ main.py
@@ -5,18 +5,29 @@ import weather_parser
 City = str(input("Enter City Name: "))
 statusCode,oridinates = callAPI.coordinates(City)
 json_ordinates = json.loads(oridinates)
+
 outList = weather_parser.parse_cities(json_ordinates)
 found_cities_num = (len(outList))
-# print((outList))
 if found_cities_num == 1:
-    #call func weather
     lan = outList[0][1]
     lon = outList[0][2]
-    # print(lan,lon)
     status_code,weather = callAPI.weather(lan,lon)
     print(weather)
-    #call func parse_weather
 elif found_cities_num > 1:
-    pass
-    #user input 
-    #call func weather
+    json_ordinates = json.loads(oridinates)
+    dic_cities = weather_parser.select_city(json_ordinates)
+    user_dislpay = weather_parser.unpack_dic(**dic_cities)
+    
+    for line in user_dislpay:
+        print(line)
+    user_input = input("Please select any one key from above: ")
+    while user_input not in dic_cities:
+        user_input = input("Wrong key, please select correct key: ")    
+    get_city = dic_cities.get(user_input)
+    print(get_city)
+    Lan,Lon = get_city[1],get_city[2]            
+    status_code,weather = callAPI.weather(Lan,Lon)
+    print(weather)
+
+else:
+    print("Please check city name!")
--- weather_parser.py
+++ weather_parser.py
@@ -15,5 +15,17 @@ def parse_cities(cities):
         outerList.append(innerList)
     return outerList
 
-def select_coordinates():
-    pass
+def select_city(cities):
+    dic_cities = {}
+    for i in range(len(cities)):
+        City = cities[i]["name"]
+        Lat = cities[i]["lat"]
+        Lon = cities[i]["lon"]
+        Country = cities[i]["country"]
+        State = cities[i]["state"]
+        dic_cities["Key"+str(i)] = [City,Lat,Lon,Country,State]
+    return dic_cities
+
+def unpack_dic(**kwargs):
+    for k,v in kwargs.items():
+        yield (k+ " - "+ ", ".join(map(str,v)))
